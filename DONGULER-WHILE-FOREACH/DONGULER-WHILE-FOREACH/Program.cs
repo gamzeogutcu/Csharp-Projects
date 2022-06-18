cf185c1b1a65cc88191a542e60fa4849a8d0c56d
Gamze Öğütcü <104031285+gamzeogutcu@users.noreply.github.com>
2022-06-19 01:18:40 +0300

    While Foreach ödevi yapıldı

---
+++ DONGULER-WHILE-FOREACH/DONGULER-WHILE-FOREACH/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace DONGULER_WHILE_FOREACH
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //While
+            //1 den başlayarak colsoledan girilen sayıya kadar (sayı dahil) ortalama hesaplayıp console'a yazdıran program
+            Console.Write("Lütfen bir sayı giriniz: ");
+            int sayi = int.Parse(Console.ReadLine());
+            int sayac = 1;
+            int toplam = 0;
+            while (sayac <= sayi)
+            {
+                toplam += sayac;
+                sayac++;
+            }
+            Console.WriteLine(toplam / sayi);
+            // a dan z ye kadar tüm harfleri console a yazdır
+            char character = 'a';
+            while(character < 'z')
+            {
+                Console.Write(character);
+                character ++;
+
+            }
+            Console.WriteLine("****Foreach***");
+            string[] arabalar = { "BMW", "Ford", "Toyota", "Nissan" };
+            foreach (var araba in arabalar)
+            {
+                Console.WriteLine(araba);
+            }
+            
+        }
+    }
+}
