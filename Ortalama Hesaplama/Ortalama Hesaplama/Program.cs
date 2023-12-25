a741f94f06283fd326e2790f6cb4bb5c8fb040eb
Gamze Öğütcü <104031285+gamzeogutcu@users.noreply.github.com>
2023-12-25 04:14:46 +0300

    Ortalama hesaplama projesi eklendi.
    Kulanıcıdan alınan derinliğe göre fibonacci serisindeki rakamların ortalamasını alıp ekrana yazdıran uygulamayı

---
+++ Ortalama Hesaplama/Ortalama Hesaplama/Program.cs
@@ -0,0 +1,30 @@
+﻿// See https://aka.ms/new-console-template for more information
+//Kulanıcıdan alınan derinliğe göre fibonacci serisindeki
+//rakamların ortalamasını alıp ekrana yazdıran uygulamayı yazınız.
+using System.Collections;
+Console.WriteLine("Lütfen pozitif bir değer giriniz: ");
+int n = Convert.ToInt32(Console.ReadLine());
+int[] sonuc = Fibonacci(ref n);
+float ortalama = Ortalama(ref sonuc,ref n);
+Console.WriteLine("Girmiş olduğunuz derinliğe göre fibonacci serisindeki rakamların ortalaması: "+ortalama);
+static int[] Fibonacci(ref int n)
+{
+    int[] fibonacciSerisi = new int[n];
+    fibonacciSerisi[0] = 1;
+    fibonacciSerisi[1] = 1;
+    for (int i = 2; i < n; i++){
+        fibonacciSerisi[i] = fibonacciSerisi[i-1] + fibonacciSerisi[i-2];
+}
+    return fibonacciSerisi;
+
+}
+static float Ortalama(ref int[] x,ref int n)
+{
+    float toplam = 0;
+   foreach(var item in x)
+    {
+        toplam += item;
+    }
+    float ortalama = toplam / n;
+    return ortalama;
+}
