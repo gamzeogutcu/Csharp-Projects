b882e61dc5bcd8591e9d8ad60ef66057ff9fe5e3
Gamze Öğütcü <104031285+gamzeogutcu@users.noreply.github.com>
2024-01-04 01:50:04 +0300

    Karakter tersten yazdırma projesi eklendi.

---
+++ Karakter-Tersten-Yazdirma/Karakter-Tersten-Yazdirma/Program.cs
@@ -0,0 +1,6 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Lütfen bir ifade giriniz: ");
+string ifade = Console.ReadLine();
+char[] ifade_ = ifade.ToCharArray();
+Array.Reverse(ifade_);
+Console.WriteLine(ifade_);
