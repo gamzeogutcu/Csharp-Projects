141e91695beb3032cc200539579753f28f793246
Gamze Öğütcü <104031285+gamzeogutcu@users.noreply.github.com>
2024-01-04 01:40:02 +0300

    Algoritma projesi eklendi

---
+++ Algoritma/Algoritma/Program.cs
@@ -0,0 +1,6 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Lütfen aralarına virgül(,) koyarak bir ifade ve sayı giriniz: ");
+string input = Console.ReadLine();
+string[] ifade = input.Split(',');
+int sayi = int.Parse(ifade[1]);
+Console.WriteLine(ifade[0].Remove(sayi - 1,1));
